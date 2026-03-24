03e825b4a22d0c34692791ad6fbbd962cece0ad7
lyaazijordan-ui <lyaazijordan@gmail.com>
2026-03-24 11:31:26 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,21 @@
+import streamlit as st
+from supabase import create_client
 
+supabase = create_client(
+    st.secrets["SUPABASE_URL"],
+    st.secrets["SUPABASE_KEY"]
+)
+
+def signup(email, password):
+    try:
+        supabase.auth.sign_up({"email": email, "password": password})
+        return True
+    except:
+        return False
+
+def login(email, password):
+    try:
+        supabase.auth.sign_in_with_password({"email": email, "password": password})
+        return True
+    except:
+        return False
